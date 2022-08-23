d35279801b53e21d9a9c7bbdda80a9905d695b4f
abhishek-dangol <abhi.dangol@gmail.com>
2022-08-23 14:55:27 -0500

    Added end points

--- copycat_api.py
+++ copycat_api.py
@@ -1,7 +1,8 @@
+from tkinter.tix import MAX
 from typing import Union
 
-from fastapi import FastAPI
-from copycat import generate_branding_snippet, generate_keywords
+from fastapi import FastAPI, HTTPException
+from copycat import generate_branding_snippet, generate_keywords, MAX_INPUT_LENGTH
 
 
 app = FastAPI()
@@ -9,13 +10,34 @@ app = FastAPI()
 
 @app.get("/generate_snippet")
 def generate_snippet_api(prompt: str):
-    # snippet = generate_branding_snippet()
-    return {"message": f"Hello CopyCat prompt: {prompt}"}
+    validate_input_length(prompt)
+    snippet = generate_branding_snippet(prompt)
+    return {"snippet": snippet, "keywords": []}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/generate_keywords")
+def generate_keywords_api(prompt: str):
+    validate_input_length(prompt)
+    keywords = generate_keywords(prompt)
+    return {"snippet": None, "keywords": keywords}
+
+
+@app.get("/generate_snippet_and_keywords")
+def generate_keywords_api(prompt: str):
+    validate_input_length(prompt)
+    snippet = generate_branding_snippet(prompt)
+    keywords = generate_keywords(prompt)
+    return {"snippet": snippet, "keywords": keywords}
+
+
+def validate_input_length(prompt: str):
+    if len(prompt) >= MAX_INPUT_LENGTH:
+        raise HTTPException(status_code=400, detail=f"The word is too long! It must be under {MAX_INPUT_LENGTH} characters")
+
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
 
 
 # python3 -m uvicorn copycat_api:app --reload
